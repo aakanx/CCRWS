ee19e129006c1bf4815ca01801236e81572dfea2
Aakanx Panchal <aakanx.panchal@queensu.ca>
2026-09-15 17:21:10 -0400

    edits

--- write-ojs.R
+++ write-ojs.R
@@ -1,8 +1,8 @@
 write_ojs <- function(f, columns) {
   str_glue('```{{ojs}}
             md`${{get_question("{columns}")}}`
-            viewof input_{columns} = make_input_{f}(employee, "{columns}")
+            viewof input_{columns} = make_input_{f}(data, "{columns}")
             html`<br>`
-            make_table(make_objects_{f}, filtered_employee, "{columns}")
+            make_table(make_objects_{f}, filtered_data, "{columns}")
             ```')
 }
